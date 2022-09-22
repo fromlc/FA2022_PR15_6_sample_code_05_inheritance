52cbd29954a6cf1c0edfad7f1d91e8d24949a7f9
L Carver <wifihum@gmail.com>
2022-09-21 17:47:39 -0700

    more code style edits for consistency

---
+++ source/Mini.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "Mini.h"
+
+using std::cout;
+
+void Mini::print() {
+	cout << this->getMake() << "\n";
+}
---
+++ source/Mini.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Automobile.h"
+
+class Mini : public Automobile {
+public:
+	void print();
+};
+
